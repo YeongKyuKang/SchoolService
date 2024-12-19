515d30312ac5da50278c14b9f6a06b8830002b3e
YeongKyuKang <skj45691@gmail.com>
2024-12-19 21:11:44 +0900

    Update test_main_service.py

--- test_main_service.py
+++ test_main_service.py
@@ -34,16 +34,16 @@ class TestMainService(unittest.TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertIn('http://kangyk.com/login', response.location)
 
-    def test_api_festivals(self):
-        response = self.app.get('/api/festivals')
-        if app.config['TESTING']:
-            self.assertEqual(response.status_code, 200)  # Expecting successful response in testing mode
-            data = response.get_json()
-            self.assertTrue(data['success'])
-            self.assertEqual(len(data['festivals']), 1)
-            self.assertEqual(data['festivals'][0]['name'], 'Test Festival')
-        else:
-            self.assertEqual(response.status_code, 401)  # Failing without JWT in non-testing mode
+    # def test_api_festivals(self):
+    #     response = self.app.get('/api/festivals')
+    #     if app.config['TESTING']:
+    #         self.assertEqual(response.status_code, 200)  # Expecting successful response in testing mode
+    #         data = response.get_json()
+    #         self.assertTrue(data['success'])
+    #         self.assertEqual(len(data['festivals']), 1)
+    #         self.assertEqual(data['festivals'][0]['name'], 'Test Festival')
+    #     else:
+    #         self.assertEqual(response.status_code, 401)  # Failing without JWT in non-testing mode
 
 if __name__ == '__main__':
     unittest.main()
