5f068d57986c986498f6d9bbe3039028dc1aab42
stiveqw <109706341+stiveqw@users.noreply.github.com>
2024-12-20 20:07:19 +0900

    test + logging

--- test_main_service.py
+++ test_main_service.py
@@ -8,7 +8,7 @@ class TestMainService(unittest.TestCase):
         self.app = app.test_client()
 
     def test_home_redirect(self):
-        response = self.app.get('/main')
+        response = self.app.get('/')
         if app.config['TESTING']:
             self.assertEqual(response.status_code, 200)  # Expecting successful response in testing mode
         else:
@@ -17,33 +17,33 @@ class TestMainService(unittest.TestCase):
     def test_course_registration_redirect(self):
         response = self.app.get('/course_registration')
         self.assertEqual(response.status_code, 302)
-        self.assertIn('http://kangyk.com/course_registration', response.location)
+        self.assertIn('course-service.course-service.svc.cluster.local/course_registration', response.location)
 
     def test_festival_redirect(self):
         response = self.app.get('/festival')
         self.assertEqual(response.status_code, 302)
-        self.assertIn('http://kangyk.com/festival', response.location)
+        self.assertIn('festival-service.festival-service.svc.cluster.local/', response.location)
 
     def test_news_redirect(self):
-        response = self.app.get('/notice')
+        response = self.app.get('/news')
         self.assertEqual(response.status_code, 302)
-        self.assertIn('http://kangyk.com/notice', response.location)
+        self.assertIn('notice-service.notice-service.svc.cluster.local/news', response.location)
 
     def test_logout_redirect(self):
         response = self.app.get('/logout')
         self.assertEqual(response.status_code, 302)
-        self.assertIn('http://kangyk.com/login', response.location)
+        self.assertIn('main-service.main-service.svc.cluster.local/main', response.location)
 
-    # def test_api_festivals(self):
-    #     response = self.app.get('/api/festivals')
-    #     if app.config['TESTING']:
-    #         self.assertEqual(response.status_code, 200)  # Expecting successful response in testing mode
-    #         data = response.get_json()
-    #         self.assertTrue(data['success'])
-    #         self.assertEqual(len(data['festivals']), 1)
-    #         self.assertEqual(data['festivals'][0]['name'], 'Test Festival')
-    #     else:
-    #         self.assertEqual(response.status_code, 401)  # Failing without JWT in non-testing mode
+    def test_api_festivals(self):
+        response = self.app.get('/api/festivals')
+        if app.config['TESTING']:
+            self.assertEqual(response.status_code, 200)  # Expecting successful response in testing mode
+            data = response.get_json()
+            self.assertTrue(data['success'])
+            self.assertEqual(len(data['festivals']), 1)
+            self.assertEqual(data['festivals'][0]['name'], 'Test Festival')
+        else:
+            self.assertEqual(response.status_code, 401)  # Failing without JWT in non-testing mode
 
 if __name__ == '__main__':
     unittest.main()
